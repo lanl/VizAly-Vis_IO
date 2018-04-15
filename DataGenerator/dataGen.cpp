097b4c4689a64f233a7fc49bb9117250ab5f0ad7
Pascal Grosset <pascal.av.grosset@gmail.com>
2018-04-15 08:50:24 -0600

    correct header and partitioning

--- DataGenerator/dataGen.cpp
+++ DataGenerator/dataGen.cpp
@@ -29,11 +29,12 @@ int main(int argc, char* argv[])
 	srand (time(NULL) + myRank);
 	
 	{
+		float simExtents[6]={0,256, 0,256, 0,256};
 		int dims[3]= {2, 2, 2};
 		int periods[3] = { 0, 0, 0 };
 		int physOrigin[3] = {0, 0, 0};
 		int physScale[3] = {256, 256, 256};
-		int numParticles = 100;
+		size_t numParticles = 10000;
 
 
 		MPI_Cart_create(Comm, 3, dims, periods, 0, &Comm);
@@ -52,14 +53,6 @@ int main(int argc, char* argv[])
         }
 
 
-		newGIO.addOctreeHeader((uint64_t)0, (uint64_t)1, (uint64_t)8);
-		uint64_t extents[6]={0,256, 0,256, 0,256};
-		float simExtents[6]={0,256, 0,256, 0,256};
-		for (int i=0; i<8; i++)
-		{
-			newGIO.addOctreeRow(i,extents, 1000, 0, i);
-		}
-		
 		
 		//
 		// Variables
@@ -80,7 +73,7 @@ int main(int argc, char* argv[])
 
 		int offsetX, offsetY, offsetZ;
 		int _8RankOffset = 128;
-		if (myRank == 0) //
+		if (myRank == 0)
 		{
 			offsetX = 0;	offsetY = 0;	offsetZ = 0;
 		}
@@ -96,7 +89,7 @@ int main(int argc, char* argv[])
 		{
 			offsetX = 0;	offsetY = _8RankOffset;	offsetZ = _8RankOffset;
 		}
-		else if (myRank == 4) //
+		else if (myRank == 4)
 		{
 			offsetX = _8RankOffset;	offsetY = 0;	offsetZ = 0;
 		}
@@ -166,13 +159,15 @@ int main(int argc, char* argv[])
 
 		float rankExtents[6];
 		int numLevels = 2;
-		int displayRank = 0;
+		int displayRank = 1;
+		int shuffle = 0;
 
 		rankExtents[0] = offsetX;	rankExtents[1] = offsetX + _8RankOffset;
 		rankExtents[2] = offsetY;	rankExtents[3] = offsetY + _8RankOffset;
 		rankExtents[4] = offsetZ;	rankExtents[5] = offsetZ + _8RankOffset;
 		Octree gioOctree(numLevels, simExtents);
 		gioOctree.buildOctree();
+		gioOctree.myRank = myRank;
 
 		int numOctreeLeaves = gioOctree.getNumNodes();
 		int numLeavesPerRank = numOctreeLeaves/numRanks;
@@ -184,15 +179,17 @@ int main(int argc, char* argv[])
 										<< rankExtents[2] << "-" << rankExtents[3] << ", "
 										<< rankExtents[4] << "-" << rankExtents[5] << std::endl;
 
-			std::cout << myRank << " ~ " << numOctreeLeaves << ", " << numLeavesPerRank << std::endl;
+			std::cout << myRank << " ~  # octree leaves: " << numOctreeLeaves << ", leaves/rank: " << numLeavesPerRank << std::endl;
 		}
+
+
 
 		//
         // Storing number of partitions per rank
         std::vector< int > myLeaves;
-        float myOctreeExtents[6];
-        myOctreeExtents[0]=myOctreeExtents[2]=myOctreeExtents[4]=std::numeric_limits<float>::max();
-        myOctreeExtents[1]=myOctreeExtents[3]=myOctreeExtents[5]=std::numeric_limits<float>::min();
+
+
+        int *leavesExtents = new int[numLeavesPerRank*6];
 
         for (int i=0; i<numLeavesPerRank; i++)
         {
@@ -201,26 +198,104 @@ int main(int argc, char* argv[])
 
             float _extents[6];
             gioOctree.getLeafExtents(leafID,_extents);
-            myOctreeExtents[0]=std::min(_extents[0], myOctreeExtents[0]);
-            myOctreeExtents[2]=std::min(_extents[2], myOctreeExtents[2]);
-            myOctreeExtents[4]=std::min(_extents[4], myOctreeExtents[4]);
 
-            myOctreeExtents[1]=std::max(_extents[1], myOctreeExtents[1]);
-            myOctreeExtents[3]=std::max(_extents[3], myOctreeExtents[3]);
-            myOctreeExtents[5]=std::max(_extents[5], myOctreeExtents[5]);
+            // Gathering extents of all leaves
+            for (int j=0; j<6; j++)
+            	leavesExtents[i*6 + j] = _extents[j];
 
-            //debugLog.addLog( std::to_string(i) + ": " + std::to_string(leafID) + ", " );
 
-			// std::cout <<myRank << " ~ " << leafID << " | " 
-			// 			<< myOctreeExtents[0] << "-" << myOctreeExtents[1] << ", "
-			// 			<< myOctreeExtents[2] << "-" << myOctreeExtents[3] << ", "
-			// 			<< myOctreeExtents[4] << "-" << myOctreeExtents[5] << std::endl;
 			if (myRank == displayRank)
+			{
 				std::cout <<myRank << " ~ " << leafID << " | " 
 							<< _extents[0] << "-" << _extents[1] << ", "
 							<< _extents[2] << "-" << _extents[3] << ", "
 							<< _extents[4] << "-" << _extents[5] << std::endl;
+			}
         }
+
+
+        
+
+
+
+        // Rearrange the particles
+        std::vector<int> partitionPosition;
+        std::vector<int> partitionCount;
+        float *_xx = &xx[0];
+        float *_yy = &yy[0];
+        float *_zz = &zz[0];
+
+        partitionCount = gioOctree.findPartition(_xx,_yy,_zz, numParticles, numLeavesPerRank, leavesExtents, partitionPosition);
+
+
+        
+
+        if (myRank == displayRank)
+			for (int i=0; i<numLeavesPerRank; i++)
+				std::cout << myRank << " : " << partitionCount[i] << std::endl;
+
+		float *_temp;
+		_temp = &xx[0];	gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+		_temp = &yy[0];	gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+		_temp = &zz[0];	gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+		_temp = &vx[0];	gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+		_temp = &vy[0];	gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+		_temp = &vz[0];	gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+		_temp = &phi[0];gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _temp, numParticles, false);
+
+		// Temp to get info
+		uint16_t *_tempMask = &mask[0];
+		gioOctree.fillArray(numLeavesPerRank, partitionCount, _tempMask, numParticles);
+		//gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _tempMask, numParticles, false);
+
+		int64_t *_tempId = &id[0];
+		gioOctree.reorganizeArray(numLeavesPerRank, partitionCount, partitionPosition, _tempId, numParticles, false);		
+		
+
+
+		//MPI_Barrier(MPI_COMM_WORLD);
+		int * nodesPerLeaves = new int[numOctreeLeaves];
+        int *myLeavesCount;  myLeavesCount=&partitionCount[0];
+
+        //for (int i=0; i<numLeavesPerRank; i++)
+        //	std::cout << myRank << " ~ " << myLeavesCount[i] << std::endl;
+
+        MPI_Allgather( myLeavesCount, numLeavesPerRank, MPI_INT,  nodesPerLeaves, numLeavesPerRank, MPI_INT,  MPI_COMM_WORLD); 
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        // if (myRank == displayRank)
+        // 	for (int i=0; i<numOctreeLeaves; i++)
+        // 	std::cout << i << " ~ " << (uint64_t)nodesPerLeaves[i] << std::endl;
+
+
+        newGIO.addOctreeHeader((uint64_t)shuffle, (uint64_t)numLevels, (uint64_t)numOctreeLeaves);
+		uint64_t extents[6]={0,256, 0,256, 0,256};
+
+		int leafCount = 0;
+		for (int r=0; r<numRanks; r++)
+		{
+			uint64_t offsetInRank = 0;
+			for (int l=0; l<numLeavesPerRank; l++)
+        	{
+        		int leafID = r*numleavesForMyRank + l;
+
+
+            	float __extents[6];
+            	gioOctree.getLeafExtents(leafID,__extents);
+
+            	uint64_t _leafExtents[6];
+            	for (int i=0; i<6; i++)
+            		_leafExtents[i] = (uint64_t) round(__extents[i]);
+            	//// 	newGIO.addOctreeRow(i,extents, 100, 0, i);
+            	if (myRank == displayRank)
+            		std::cout << nodesPerLeaves[leafCount] << std::endl;
+            	newGIO.addOctreeRow(leafCount,_leafExtents, nodesPerLeaves[leafCount], offsetInRank, r);
+
+            	leafCount++;
+            	offsetInRank += nodesPerLeaves[leafCount];
+        	}
+		}
+
 
         newGIO.write();
 
