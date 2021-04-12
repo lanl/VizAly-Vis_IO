7f62ee3280886216dfce5c33b4b834c66f3eb5f9
Pascal Grosset <pascalgrosset@gmail.com>
2021-04-12 14:23:11 -0600

    timeseries analysis

---
+++ vel-test-400/spectrum/spectrum7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=galton
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=16
+date
+mkdir -p /projects/groups/vizproject/dssdata/Exasky/vel_run/vel-test-ts-401/analysis/spectrum
+cd /projects/groups/vizproject/dssdata/Exasky/vel_run/vel-test-ts-401/analysis/spectrum
+source /projects/exasky/HACC/trunk/env/bashrc.darwin.cpu
+mpirun /projects/exasky/HACC/trunk/darwin.cpu/mpi/bin/hacc_pk_gio_auto_rsd /projects/exasky/pascal-projects/VizAly-Foresight/inputs/hacc/halo_inputs/indat.params -n /projects/groups/vizproject/dssdata/Exasky/vel_run/vel-test-ts-401/reduction/cbench/decompressed_files/sz_vel_.01__m000.full.mpicosmo.401 sz_vel_.01 499
+date
---
+++ vel-test-499/spectrum/spectrum0.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#SBATCH --partition=galton
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=16
+date
+mkdir -p //projects/groups/vizproject/dssdata/Exasky/vel_run/vel-test-4/analysis/spectrum
+cd //projects/groups/vizproject/dssdata/Exasky/vel_run/vel-test-4/analysis/spectrum
+source /projects/exasky/HACC/trunk/env/bashrc.darwin.cpu
+mpirun /projects/exasky/HACC/trunk/darwin.cpu/mpi/bin/hacc_pk_gio_auto_rsd /projects/exasky/pascal-projects/VizAly-Foresight/inputs/hacc/halo_inputs/indat.params -n /projects/groups/vizproject/dssdata/cosmo/Argonne_L360_HACC001/STEP499/m000.full.mpicosmo.499 orig 499
+date
